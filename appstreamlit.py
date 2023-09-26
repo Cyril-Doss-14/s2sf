539a2641a4052de989d3ec5ec57677126b82582e
CYRIL DOSS I <127850359+Cyril-Doss-14@users.noreply.github.com>
2023-09-27 02:17:53 +0530

    Update appstreamlit.py

--- appstreamlit.py
+++ appstreamlit.py
@@ -49,7 +49,7 @@ def predict_realtime():
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
 
     while True:
         ret, frame = cap.read()
