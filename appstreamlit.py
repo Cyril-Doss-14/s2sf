e595ec143d9222de3d29cfb530a4d675b7aaec58
CYRIL DOSS I <127850359+Cyril-Doss-14@users.noreply.github.com>
2023-09-27 02:23:45 +0530

    Update appstreamlit.py

--- appstreamlit.py
+++ appstreamlit.py
@@ -13,9 +13,6 @@ st.set_page_config(page_title="Sign2Speech Translation", page_icon="🤟")
 # Load the trained model for image prediction
 image_model = load_model('sign2speech_best_model.h5')
 
-# Load the trained model for real-time prediction
-realtime_model = tf.keras.models.load_model('sign2speech_best_model.h5')
-
 # Define the classes corresponding to the signs ('0' to 'Z')
 classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
@@ -53,12 +50,7 @@ def predict_realtime():
 
     if not cap.isOpened():
         st.write("Error: Camera not found.")
-    else:
-    # Load the model here
-        image_model = load_model('sign2speech_best_model.h5')
-    
-    # Rest of your code for real-time prediction
-
+        return  # Exit the function if the camera is not found
 
     while True:
         ret, frame = cap.read()
@@ -89,7 +81,7 @@ def predict_realtime():
             hand_frame_resized = cv2.resize(hand_frame, (224, 224))
             hand_frame_normalized = hand_frame_resized / 255.0
 
-            prediction = realtime_model.predict(np.expand_dims(hand_frame_normalized, axis=0))
+            prediction = image_model.predict(np.expand_dims(hand_frame_normalized, axis=0))
             predicted_label = np.argmax(prediction)
             predicted_sign = classes[predicted_label]
             confidence = prediction[0][predicted_label]
@@ -111,7 +103,6 @@ def predict_realtime():
     cv2.destroyAllWindows()
 
 # Create a Streamlit web application
-# Create a Streamlit web application
 st.title('Sign Language Prediction')
 
 # Choose an option
@@ -130,5 +121,3 @@ elif option == 'Predict in Real Time':
     st.sidebar.header('Real-Time Prediction')
     st.text("Camera is on. Hold up a sign for prediction. Press q to close the camera.👍")
     predict_realtime()
-
-
